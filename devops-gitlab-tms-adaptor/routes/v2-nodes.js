126a06b1c22fb4debb34bd6035eda89a83506b41
Laszlo Kajan <laszlo.kajan@roche.com>
2021-05-24 12:20:36 +0200

    Added routes/v2-nodes.js logging

--- devops-gitlab-tms-adaptor/routes/v2-nodes.js
+++ devops-gitlab-tms-adaptor/routes/v2-nodes.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+var debug = require('debug')('devops-gitlab-tms-adaptor:routes-v2-nodes');
 var express = require('express');
 var router = express.Router();
 
@@ -94,57 +95,61 @@ router.get('/', function (req, res, next) {
 });
 
 // API https://api.sap.com/api/TMS_v2/resource
-//  GET /v2/nodes/2/transportRequests?status=in
-//  GET /v2/nodes/{nodeId}/transportRequests?status={in,ru,re,su,wa,er,fa,de}
+//  GET /v2/nodes/2/transportRequests?status=in%2cre
+//  GET /v2/nodes/{nodeId}/transportRequests?status={in,ru,re,su,wa,er,fa,de}: `in` - Initial  * `ru` - Running  * `re` - Repeatable  * `su` - Succeeded  * `wa` - Warning  * `er` - Error  * `fa` - Fatal  * `de` - Deleted
 router.get('/:nodeId(\\d+)/transportRequests', async function (req, res, next) {
 
-    // req.params.nodeId - will always be >2< given the nodes above
-    // req.query.status - will always be 'in'
-    const glProjects = JSON.parse(process.env.GITLAB_PROJECTS); // [4396]
+    let transportRequests = [];
     let transportRequestPosition = 1;
+    let statusArray = req.query.status ? decodeURIComponent(req.query.status).split(',') : [];
 
-    let promises = glProjects.map(glProjectId => {
-        const tagsUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${glProjectId}/repository/tags?search=^v`;
-        return axios.get(tagsUrl, {
-            headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_READ_API}` }
-        });
-    });
+    if (req.params.nodeId == '2' && statusArray.filter(status => status === 'in').length ) {
+        const glProjects = JSON.parse(process.env.GITLAB_PROJECTS); // [4396]
 
-    let responses = await Promise.all(promises);
-
-    const transportRequests = responses.reduce((acc, response) => {
-        const _transportRequests = response.data.map((elem) => {
-            // Map commit to tr. req. id:
-            //  Convert first 13 characters (52 bits) of commit to transport request id, which 53 bits precision:
-            //  parseInt("1dabe74c48a320db1d3c8a04d7fba4ec4025097f".substr(0, 13), 16)
-            function commit2id(__commit) { return parseInt(__commit.substr(0, 13), 16); }
-            const trId = commit2id(elem.commit.id);
-            const trEntryId = commit2id(elem.commit.short_id);
-            const trDescription = `project: 4396, tag: ${elem.name}, commit: ${elem.commit.id}`.
-                replace(/[^\w ._~:\/?#[\]@!$&()*+,;=%-]/g, '_'); // C.f. devops-scripts/tms-upload
-
-            return {
-                "id": trId,           // integer($int64) in the API definition, but we really have only 53 bits, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
-                "status": "initial",
-                "archived": false,
-                "position": transportRequestPosition++,
-                "createdBy": elem.commit.committer_email,
-                "createdAt": elem.commit.committed_date,    // "2021-04-14T12:24:41.000+00:00"
-                "description": trDescription,
-                "origin": "DGT",                            // /dev/ node!
-                "entries": [
-                    {
-                        "id": trEntryId,
-                        "storageType": "FILE",
-                        "contentType": "MTA",
-                        "uri": `${trEntryId}`   // "4378"
-                    }
-                ]
-            };
+        let promises = glProjects.map(glProjectId => {
+            const tagsUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${glProjectId}/repository/tags?search=^v`;
+            debug(`axios GET ${tagsUrl}`);
+            return axios.get(tagsUrl, {
+                headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_READ_API}` }
+            });
         });
 
-        return acc.concat(_transportRequests);
-    }, []);
+        let responses = await Promise.all(promises);
+
+        transportRequests = responses.reduce((acc, response) => {
+            const _transportRequests = response.data.map((elem) => {
+                // Map commit to tr. req. id:
+                //  Convert first 13 characters (52 bits) of commit to transport request id, which 53 bits precision:
+                //  parseInt("1dabe74c48a320db1d3c8a04d7fba4ec4025097f".substr(0, 13), 16)
+                function commit2id(__commit) { return parseInt(__commit.substr(0, 13), 16); }
+                const trId = commit2id(elem.commit.id);
+                const trEntryId = commit2id(elem.commit.short_id);
+                const trDescription = `project: 4396, tag: ${elem.name}, commit: ${elem.commit.id}`.
+                    replace(/[^\w ._~:\/?#[\]@!$&()*+,;=%-]/g, '_'); // C.f. devops-scripts/tms-upload
+
+                return {
+                    "id": trId,           // integer($int64) in the API definition, but we really have only 53 bits, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+                    "status": "initial",
+                    "archived": false,
+                    "position": transportRequestPosition++,
+                    "createdBy": elem.commit.committer_email,
+                    "createdAt": elem.commit.committed_date,    // "2021-04-14T12:24:41.000+00:00"
+                    "description": trDescription,
+                    "origin": "DGT",                            // Give /dev/ node
+                    "entries": [
+                        {
+                            "id": trEntryId,
+                            "storageType": "FILE",
+                            "contentType": "MTA",
+                            "uri": `${trEntryId}`   // "4378"
+                        }
+                    ]
+                };
+            });
+
+            return acc.concat(_transportRequests);
+        }, []);
+    }
 
     res.send({
         "transportRequests": transportRequests
