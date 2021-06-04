5ed51eb2e5f299584b4f6f3fe9e919e948e0ff63
Laszlo Kajan <laszlo.kajan@roche.com>
2021-06-04 21:52:01 +0200

    Implemented PRD /import

--- devops-gitlab-tms-adaptor/routes/v2-nodes.js
+++ devops-gitlab-tms-adaptor/routes/v2-nodes.js
@@ -23,13 +23,13 @@ async function getCommit(glProjects, commitHashFragment, processEnv) {
     return successResponses[0].data;
 }
 
-async function getDeploymentJob(commit, processEnv) {
+async function getDeploymentJob(commit, processEnv, environment) {
 
     const committed_date = commit.committed_date;
 
     // Use committed_date, environment and status to get deployment, return deployment job number as action id
     const deploymentUrl = `https://${processEnv.GITLAB_HOST}/api/v4/projects/${commit.project_id}/deployments?` +
-        `environment=Test&status=success&updated_after=${encodeURIComponent(committed_date)}&` +
+        `environment=${environment}&status=success&updated_after=${encodeURIComponent(committed_date)}&` +
         'order_by=created_at&sort=desc';
     debug(`axios GET ${deploymentUrl}`);
     const deploymentResponse = await axios.get(deploymentUrl, {
@@ -277,7 +277,7 @@ router.post('/:nodeId(\\d+)/transportRequests/import', async function (req, res,
                     // Find which project the transport request (= commit) belongs to:
                     //  use transport id to get committed_date from commit
                     const commit = await getCommit(glProjects, commitHashFragment, process.env);
-                    const deployment = await getDeploymentJob(commit, process.env);
+                    const deployment = await getDeploymentJob(commit, process.env, 'Test');
 
                     res.send({
                         "actionId": deployment.deployable.id,
@@ -345,6 +345,111 @@ router.post('/:nodeId(\\d+)/transportRequests/import', async function (req, res,
                     break;
                 }
 
+            case "4": // PRD
+                {
+                    const commit = await getCommit(glProjects, commitHashFragment, process.env);
+
+                    // Find unmerged /^quality-/ branch of commit.
+                    const branchUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${commit.project_id}/repository/` +
+                        `branches?search=^quality-`;
+                    debug(`axios GET ${branchUrl}`);
+                    const branchesResponse = await axios.get(branchUrl, {
+                        headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_API}` }
+                    });
+                    const branches = branchesResponse.data.filter(elem => elem.merged === false && elem.commit.id === commit.id);
+                    if (branches.length !== 1) { throw new Error(`expected 1 branch, found ${branches.length}`); }
+
+                    // Create merge request.
+                    const createMRUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${commit.project_id}/merge_requests?` +
+                        `source_branch=${encodeURIComponent(branches[0].name)}&target_branch=production&` +
+                        `title=${encodeURIComponent('Merge ' + branches[0].name + ' to production')}`;
+                    debug(`axios POST ${createMRUrl}`);
+                    const createMRResponse = await axios.post(createMRUrl, '', {
+                        headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_API}` }
+                    });
+
+                    // Wait for merge request to go into 'acceptable' state
+                    {
+                        let getMRResponse, countdown = 33, can_be_merged = false;
+                        while (countdown-- > 0) {
+                            const getMRUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${commit.project_id}/` +
+                                `merge_requests/${createMRResponse.data.iid}`;
+                            debug(`axios GET ${getMRUrl}`);
+                            getMRResponse = await axios.get(getMRUrl, {
+                                headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_API}` }
+                            });
+
+                            if (getMRResponse.status === 200 && getMRResponse.data.state === 'opened' &&
+                                getMRResponse.data.merge_status === "can_be_merged") { can_be_merged = true; break; }
+                            //
+                            await utils.sleep(1000);
+                        }
+                        if (!can_be_merged) { throw new Error(`merge request ${createMRResponse.data.iid} can't be merged`); }
+                    }
+
+                    // Accept merge request with automatic merge on successful pipeline execution.
+                    const acceptMRUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${commit.project_id}/merge_requests/` +
+                        `${createMRResponse.data.iid}/merge?merge_when_pipeline_succeeds=true&sha=${commit.id}`;
+                    debug(`axios PUT ${acceptMRUrl}`);
+                    const acceptMRResponse = await axios.put(acceptMRUrl, '', {
+                        headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_API}` }
+                    });
+
+                    // Wait for merge request state to become 'merged'.
+                    {
+                        let getMRResponse, countdown = 33;
+                        while (countdown-- > 0) {
+                            const getMRUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${commit.project_id}/` +
+                                `merge_requests/${createMRResponse.data.iid}`;
+                            debug(`axios GET ${getMRUrl}`);
+                            getMRResponse = await axios.get(getMRUrl, {
+                                headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_API}` }
+                            });
+
+                            if (getMRResponse.status === 200 && getMRResponse.data.state === 'merged') { break; }
+                            //
+                            await utils.sleep(1000);
+                        }
+                        if (getMRResponse.data.state !== 'merged') { throw new Error(`failed to find merged merge request ${createMRResponse.data.iid}`); }
+                    }
+
+                    // Now the 'production' pipeline will be created.
+                    let pipelineResponse;
+                    {
+                        let countdown = 7, foundIt = false;
+                        while (countdown-- > 0) {
+                            await utils.sleep(1000);
+
+                            const pipelineUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${commit.project_id}/pipelines?` +
+                                `ref=production&sha=${commit.id}&order_by=id&sort=desc`;
+                            debug(`axios GET ${pipelineUrl}`);
+                            pipelineResponse = await axios.get(pipelineUrl, {
+                                headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_API}` }
+                            });
+
+                            if (pipelineResponse.status === 200 && pipelineResponse.data.length >= 1) { foundIt = true; break; }
+                        }
+                        if (!foundIt) { throw new Error(`failed to find pipeline ref=production&sha=${commit.id} of project ${commit.project_id}`); }
+                    }
+                    // Return resulting pipeline's 'deploy to Production' job id
+                    const pipelineJobUrl = `https://${process.env.GITLAB_HOST}/api/v4/projects/${commit.project_id}/pipelines/` +
+                        `${pipelineResponse.data[0].id}/jobs`;
+                    debug(`axios GET ${pipelineJobUrl}`);
+                    const plJobResponse = await axios.get(pipelineJobUrl, {
+                        headers: { 'Authorization': `Bearer ${process.env.GITLAB_ACCESS_API}` }
+                    });
+
+                    const deployToProdJobs = plJobResponse.data.filter(elem => elem.name === 'deploy to Production');
+                    if (deployToProdJobs.length > 1) { throw new Error(`expected 1 'deploy to Quality' job, got ${deployToProdJobs.length}`); }
+                    const deployToProdJob = deployToProdJobs[0];
+
+                    res.send({
+                        "actionId": deployToProdJob.id,
+                        "monitoringURL": `/v2/actions/${deployToProdJob.id}`
+                    });
+                    break;
+                }
+
             default:
                 throw new Error('unimplemented');
         }
@@ -357,37 +462,44 @@ router.post('/:nodeId(\\d+)/transportRequests/import', async function (req, res,
 router.get('/:nodeId(\\d+)/transportRequests/:trId(\\d+)/logs', async function (req, res, next) {
     try {                                                       // https://expressjs.com/en/guide/error-handling.html
         const glProjects = utils.getGlProjects();
+        let environment;
 
         switch (req.params.nodeId) {
             case "2":   // CONS aka. TEST: should already have been imported
-                const trId = Number(req.params.trId);
-                const commitHashFragment = trId.toString(16).padStart(13, '0');
-
-                // Find job of environment=Test deployment given the commit hash fragment
-                const commit = await getCommit(glProjects, commitHashFragment, process.env);
-                const deployment = await getDeploymentJob(commit, process.env);
-                const tmStatus = utils.getTmStatus(deployment.deployable.status);
-
-                res.send({
-                    "logs": [
-                        {
-                            "actionId": deployment.deployable.id,
-                            "actionType": "I",
-                            "status": tmStatus,
-                            "actionStartedAt": deployment.deployable.started_at,
-                            "actionTriggeredBy": "d7ad0010-09a7-4916-be8d-1166c653e0c7", // TODO: take some ID from access token
-                            "actionTriggeredByNamedUser": deployment.deployable.user.username,
-                            "messages": [],
-                            "entities": []
-                        }
-                    ]
-                });
-                // Return deployment job status as action status
-                break;
-
+                environment = 'Test'; break;
+            case "3":
+                environment = 'Quality'; break;
+            case "4":
+                environment = 'Production'; break;
             default:
                 throw new Error('unimplemented');
         }
+        const trId = Number(req.params.trId);
+        const commitHashFragment = trId.toString(16).padStart(13, '0');
+
+        // Find job of environment=Test deployment given the commit hash fragment
+        const commit = await getCommit(glProjects, commitHashFragment, process.env);
+        const deployment = await getDeploymentJob(commit, process.env, environment);
+        let logs = [];
+        if (deployment) {
+            const tmStatus = utils.getTmStatus(deployment.deployable.status);
+
+            logs = [
+                {
+                    "actionId": deployment.deployable.id,
+                    "actionType": "I",
+                    "status": tmStatus,
+                    "actionStartedAt": deployment.deployable.started_at,
+                    "actionTriggeredBy": "d7ad0010-09a7-4916-be8d-1166c653e0c7", // TODO: take some ID from access token
+                    "actionTriggeredByNamedUser": deployment.deployable.user.username,
+                    "messages": [],
+                    "entities": []
+                }
+            ];
+        }
+        res.send({
+            "logs": logs
+        });
     } catch (err) {
         next(err);
     }
